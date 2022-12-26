3208eda66090d1e477250f3a74a1579d13b1004f
ksmai <kasingmai@gmail.com>
2022-12-26 21:13:37 +0800

    update

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# bind \t accept-autosuggestion
+
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
