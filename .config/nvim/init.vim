295db513add51c2c5bd82571a919f66270015b93
ksmai <kasingmai@gmail.com>
2023-04-23 16:40:11 +0800

    nvim neotree

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -3,7 +3,6 @@
 call plug#begin()
 
 Plug 'andymass/vim-matchup'
-Plug 'preservim/nerdtree'
 Plug 'mawkler/modicator.nvim'
 
 Plug 'tpope/vim-sleuth'
@@ -20,19 +19,6 @@ syntax on
 
 " vim-matchup
 let g:matchup_matchparen_offscreen = {'method': 'popup'}
-
-" NERDTree
-nnoremap <silent> <C-n> :NERDTreeToggle<CR>
-
-" Exit Vim if NERDTree is the only window left.
-autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() |
-    \ quit | endif
-
-" If another buffer tries to replace NERDTree, put it in the other window, and bring back NERDTree.
-autocmd BufEnter * if bufname('#') =~ 'NERD_tree_\d\+' && bufname('%') !~ 'NERD_tree_\d\+' && winnr('$') > 1 |
-    \ let buf=bufnr() | buffer# | execute "normal! \<C-W>w" | execute 'buffer'.buf | endif
-
-let g:NERDTreeShowHidden = 1
 
 " quick fix window
 function! ToggleQuickFix()
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -707,7 +707,10 @@ return {
 
     {
         'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        dependencies = {
+            'nvim-tree/nvim-web-devicons',
+            { "RRethy/nvim-base16" },
+        },
         opts = {
             winbar = {
                 lualine_c = {
@@ -727,4 +730,32 @@ return {
         opts = {
         },
     },
+
+    {
+        "MunifTanjim/nui.nvim",
+        lazy = true,
+    },
+
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v2.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+        },
+        keys = {
+            { "<C-n>", "<cmd>Neotree toggle<cr>", mode = "n", noremap = true, silent = true },
+        },
+        opts = {
+            filesystem = {
+                filtered_items = {
+                    hide_dotfiles = false,
+                },
+            },
+        },
+        init = function()
+            vim.g.neo_tree_remove_legacy_commands = 1
+        end,
+    }
 }
