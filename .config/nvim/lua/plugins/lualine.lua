1432b3d3b93e25d496c1d32c7fe92bb9d8612fa6
ksmai <kasingmai@gmail.com>
2023-06-03 13:53:24 +0800

    nvim show winbar in inactive windows

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -40,32 +40,35 @@ local function lsp_client_names()
     return table.concat(buf_client_names, ", ")
 end
 
+local function lsp_status()
+    if #vim.lsp.get_active_clients({bufnr = 0}) == 0 then return "" end
+
+    local lsp = vim.lsp.util.get_progress_messages()[1]
+    if lsp then
+        local name = lsp.name or ""
+        local msg = lsp.message or ""
+        local percentage = lsp.percentage or 0
+        local title = lsp.title or ""
+        return string.format(" %%<%s: %s %s (%s%%%%) ", name, title, msg,
+                             percentage)
+    end
+
+    return lsp_client_names()
+end
+
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = {'nvim-tree/nvim-web-devicons', "tpope/vim-fugitive"},
     opts = {
+        options = {theme = 'gruvbox'},
         sections = {lualine_b = {"FugitiveStatusline", 'diff', 'diagnostics'}},
         winbar = {
             lualine_c = {"navic", color_correction = nil, navic_opts = nil},
-            lualine_y = {
-                function()
-                    if #vim.lsp.get_active_clients({bufnr = 0}) == 0 then
-                        return ""
-                    end
-
-                    local lsp = vim.lsp.util.get_progress_messages()[1]
-                    if lsp then
-                        local name = lsp.name or ""
-                        local msg = lsp.message or ""
-                        local percentage = lsp.percentage or 0
-                        local title = lsp.title or ""
-                        return string.format(" %%<%s: %s %s (%s%%%%) ", name,
-                                             title, msg, percentage)
-                    end
-
-                    return lsp_client_names()
-                end
-            }
+            lualine_y = {lsp_status}
+        },
+        inactive_winbar = {
+            lualine_c = {"navic", color_correction = nil, navic_opts = nil},
+            lualine_y = {lsp_status}
         }
     }
 }
