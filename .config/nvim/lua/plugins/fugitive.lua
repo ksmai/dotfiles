b430b48bc269067083abfd4682c4ffc0a2408f0c
ksmai <kasingmai@gmail.com>
2023-09-16 19:43:11 +0800

    nvim: toggle fugitive git status with <C-s>

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -3,6 +3,30 @@ return {
     dependencies = {{"tpope/vim-rhubarb"}},
     keys = {
         {
+            "<C-s>",
+            function()
+                local wins = vim.api.nvim_list_wins()
+                local cur = vim.api.nvim_get_current_win()
+                for _, win in ipairs(wins) do
+                    local success, status =
+                        pcall(vim.api.nvim_win_get_var, win, 'fugitive_status')
+                    if success then
+                        if status ~= nil and status ~= "" then
+                            vim.api.nvim_win_close(win, false)
+                            if vim.api.nvim_win_is_valid(cur) then
+                                vim.api.nvim_set_current_win(cur)
+                            end
+                            return
+                        end
+                    end
+                end
+                vim.cmd('tab Git')
+            end,
+            mode = "n",
+            noremap = true,
+            silent = true,
+            desc = "Toggle git status"
+        }, {
             "<leader>gs",
             "<cmd>tab Git<cr>",
             mode = "n",
