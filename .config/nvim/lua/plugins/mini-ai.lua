666d056d4b1f3bc01b011f584bbe9463478907b5
ksmai <24501900+ksmai@users.noreply.github.com>
2025-09-13 12:01:17 +0800

    nvim: change to stable branch

--- .config/nvim/lua/plugins/mini-ai.lua
+++ .config/nvim/lua/plugins/mini-ai.lua
@@ -1,7 +1,12 @@
 return {
 	"nvim-mini/mini.ai",
+	-- After this commit: https://github.com/nvim-mini/mini.ai/commit/e66a71449bdf719b4b9c96e0cd7b7d6cc4112f88
+	-- @function.outer might match multiple functions in python at the same
+	-- time if there are function decorators in the code, so we temporarily
+	-- switch to the stable branch until it is resolved. If the stable branch
+	-- later also has this issue, please switch to tag v0.16.0.
 	-- version = false,
-	version = "v0.16.0",
+	branch = "stable",
 	dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
 	config = function()
 		local mini_ai = require("mini.ai")
