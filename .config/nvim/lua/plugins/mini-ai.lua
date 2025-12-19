09be963f0a2d5424ae194eb19218d12ff0b4f89f
ksmai <24501900+ksmai@users.noreply.github.com>
2025-12-19 16:33:46 +0800

    nvim: update treesitter to main branch

--- .config/nvim/lua/plugins/mini-ai.lua
+++ .config/nvim/lua/plugins/mini-ai.lua
@@ -1,14 +1,6 @@
 return {
 	"nvim-mini/mini.ai",
-	-- After this commit: https://github.com/nvim-mini/mini.ai/commit/e66a71449bdf719b4b9c96e0cd7b7d6cc4112f88
-	-- @function.outer might match multiple functions in python at the same
-	-- time if there are function decorators in the code, so we temporarily
-	-- switch to the stable branch until it is resolved. If the stable branch
-	-- later also has this issue, please switch to tag v0.16.0.
-	-- e.g. using vaf on a decorated function will select both the decorated function and the next function
-	-- TODO: try using treesitter main branch?
-	-- version = false,
-	branch = "stable",
+	version = false,
 	dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
 	config = function()
 		local mini_ai = require("mini.ai")
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,109 +1,78 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	version = false, -- last release is way too old and doesn't work on Windows
+	lazy = false,
 	build = ":TSUpdate",
-	event = { "BufReadPost", "BufNewFile" },
+	branch = "main",
+
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter-textobjects",
-		{ "RRethy/nvim-treesitter-textsubjects" },
 	},
-	keys = {
-		{ "<cr>", desc = "Increment selection" },
-		{ "<bs>", desc = "Decrement selection", mode = "x" },
-	},
-	opts = {
-		ensure_installed = {
-			"bash",
-			"comment",
-			"css",
-			"diff",
-			"dockerfile",
-			"fish",
-			"git_config",
-			"git_rebase",
-			"gitattributes",
-			"gitcommit",
-			"gitignore",
-			"glsl",
-			"html",
-			"javascript",
-			"json",
-			"lua",
-			"make",
-			"markdown",
-			"markdown_inline",
-			"mermaid",
-			"pug",
-			"python",
-			"regex",
-			"rust",
-			"scss",
-			"sql",
-			"svelte",
-			"tsx",
-			"typescript",
-			"vue",
-			"yaml",
-			"c_sharp",
-		},
-		highlight = {
-			enable = true,
 
-			-- disable highlight in minified files
-			disable = function(lang, buf)
+	config = function()
+		vim.api.nvim_create_autocmd("FileType", {
+			group = vim.api.nvim_create_augroup("SetupTreesitter", { clear = true }),
+
+			pattern = {
+				"bash",
+				"css",
+				"diff",
+				"dockerfile",
+				"fish",
+				"gitconfig",
+				"gitrebase",
+				"gitattributes",
+				"gitcommit",
+				"gitignore",
+				"glsl",
+				"go",
+				"html",
+				"javascript",
+				"javascriptreact",
+				"json",
+				"lua",
+				"make",
+				"markdown",
+				"mermaid",
+				"python",
+				"rust",
+				"scss",
+				"sql",
+				"svelte",
+				"typescript",
+				"typescriptreact",
+				"vue",
+				"yaml",
+			},
+
+			callback = function(args)
+				local lang = vim.treesitter.language.get_lang(args.match) or args.match
+				local buf = args.buf
+
+				-- disable in minified files
 				local line_count = vim.api.nvim_buf_line_count(buf)
 				local file_size = vim.api.nvim_buf_get_offset(buf, line_count)
-				return file_size / line_count > 10000
+				if file_size / line_count > 10000 then
+					vim.notify("[treesitter] ignoring minified file", vim.log.levels.WARN)
+					return
+				end
+
+				require("nvim-treesitter").install({ lang })
+
+				if vim.treesitter.query.get(lang, "highlights") then
+					vim.treesitter.start(buf, lang)
+				end
+
+				if vim.treesitter.query.get(lang, "folds") then
+					vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+					vim.wo[0][0].foldmethod = "expr"
+					vim.wo[0][0].foldlevel = 99
+				end
+
+				if vim.treesitter.query.get(lang, "indents") then
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end
 			end,
-		},
-		indent = { enable = true },
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<cr>",
-				node_incremental = "<cr>",
-				scope_incremental = false,
-				node_decremental = "<bs>",
-			},
-		},
-		textobjects = {
-			move = {
-				enable = true,
-				set_jumps = true,
-				goto_next_start = {
-					["]]"] = { query = { "@class.outer", "@function.outer" } },
-				},
-				goto_next_end = {
-					["]["] = { query = { "@class.outer", "@function.outer" } },
-				},
-				goto_previous_start = {
-					["[["] = { query = { "@class.outer", "@function.outer" } },
-				},
-				goto_previous_end = {
-					["[]"] = { query = { "@class.outer", "@function.outer" } },
-				},
-			},
-			lsp_interop = {
-				enable = true,
-				border = "none",
-				floating_preview_opts = {},
-				peek_definition_code = {
-					["<leader>cF"] = "@function.outer",
-					["<leader>cC"] = "@class.outer",
-				},
-			},
-		},
-		textsubjects = {
-			enable = true,
-			keymaps = {
-				["<cr>"] = "textsubjects-smart",
-				["a<cr>"] = "textsubjects-container-outer",
-				["i<cr>"] = "textsubjects-container-inner",
-			},
-		},
-	},
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
+		})
 
 		vim.hl.priorities.semantic_tokens = 95
 	end,
