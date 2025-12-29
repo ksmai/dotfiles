4fd9dffadfe6a790072196c5ee2b4016c4ba5f20
ksmai <24501900+ksmai@users.noreply.github.com>
2025-12-29 18:00:22 +0800

    nvim: avoid luasnip from being active forever

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -14,6 +14,21 @@ return {
 			for lang, snips in pairs(snippets) do
 				ls.add_snippets(lang, snips)
 			end
+
+			-- https://github.com/L3MON4D3/LuaSnip/issues/258#issuecomment-1429989436
+			vim.api.nvim_create_autocmd("ModeChanged", {
+				pattern = "*",
+				group = vim.api.nvim_create_augroup("UnlinkLuaSnip", { clear = true }),
+				callback = function()
+					if
+						((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+						and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+						and not require("luasnip").session.jump_active
+					then
+						require("luasnip").unlink_current()
+					end
+				end,
+			})
 		end,
 	},
 }
