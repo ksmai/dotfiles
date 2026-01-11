55fe5a01ccba0082c02b57f5373b892ba0841d03
ksmai <24501900+ksmai@users.noreply.github.com>
2026-01-11 23:06:08 +0800

    nvim: also match middle of word with mini.jump2d

--- .config/nvim/lua/plugins/mini-jump2d.lua
+++ .config/nvim/lua/plugins/mini-jump2d.lua
@@ -24,33 +24,14 @@ return {
 						return
 					end
 
-					local edge_only = string.match(char, "[%l%d]")
 					char = vim.fn.escape(char, "\\")
-
-					local pat = {
-						[[\V\C\(]],
-					}
 
 					if string.match(char, "%l") then
 						local upper = string.upper(char)
-						table.insert(pat, upper)
-						table.insert(pat, "\\|")
 						char = "\\[" .. char .. upper .. "]"
 					end
 
-					if edge_only then
-						table.insert(pat, [[\(\<\|_\+\)\zs]])
-						table.insert(pat, char)
-						table.insert(pat, [[\|]])
-						table.insert(pat, char)
-						table.insert(pat, [[\ze_\*\>]])
-					else
-						table.insert(pat, char)
-					end
-
-					table.insert(pat, [[\)]])
-
-					opts.spotter = jump2d.gen_spotter.vimpattern(table.concat(pat))
+					opts.spotter = jump2d.gen_spotter.vimpattern([[\V\C]] .. char)
 				end
 				opts.hooks = { before_start = before_start }
 
