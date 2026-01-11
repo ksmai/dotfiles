25155c70dba4ebcda91f935f9f22f2cdbb9804df
ksmai <24501900+ksmai@users.noreply.github.com>
2026-01-11 12:54:15 +0800

    script: backup ac to dropbox

--- script/backup_ac.py
+++ script/backup_ac.py
@@ -2,6 +2,7 @@ import json
 import os
 from datetime import datetime
 
+import dropbox
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -10,7 +11,7 @@ from googleapiclient.http import MediaFileUpload
 
 
 def backup_ac_to_google(config):
-    print("Uploading to google...")
+    print("Uploading to Google...")
 
     creds = None
 
@@ -35,21 +36,37 @@ def backup_ac_to_google(config):
     upload = MediaFileUpload(config["ac_file"], mimetype=config["ac_mime_type"])
     body = {
         "parents": [config["google"]["folder_id"]],
-        "name": config["backup_file_name"],
+        "name": config["backup_filename"],
     }
     results = service.files().create(media_body=upload, body=body).execute()
 
     print(f"Uploaded to Google successfully: {results}")
 
 
+def backup_to_dropbox(config):
+    print("Uploading to Dropbox...")
+
+    with open(config["dropbox"]["token_file"]) as f:
+        token = json.load(f)
+        dbx = dropbox.Dropbox(token["access_token"])
+
+    with open(config["ac_file"], "rb") as f:
+        results = dbx.files_upload(
+            f.read(), f"/{config['dropbox']['folder_name']}/{config['backup_filename']}"
+        )
+
+    print(f"Uploaded to Dropbox successfully: {results}")
+
+
 def main():
     with open("credentials/config.json", "r") as f:
         config = json.load(f)
-        config["backup_file_name"] = config["backup_file_name"].format(
+        config["backup_filename"] = config["backup_filename"].format(
             date=datetime.now().strftime("%Y%m%d")
         )
 
     backup_ac_to_google(config)
+    backup_to_dropbox(config)
 
 
 if __name__ == "__main__":
