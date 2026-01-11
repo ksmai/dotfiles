d3c870b893784c4e9563cd79174b9ea3991dd640
ksmai <24501900+ksmai@users.noreply.github.com>
2026-01-11 13:11:07 +0800

    script: oauth for dropbox

--- script/backup_ac.py
+++ script/backup_ac.py
@@ -1,5 +1,6 @@
 import json
 import os
+import webbrowser
 from datetime import datetime
 
 import dropbox
@@ -46,9 +47,57 @@ def backup_ac_to_google(config):
 def backup_to_dropbox(config):
     print("Uploading to Dropbox...")
 
-    with open(config["dropbox"]["token_file"]) as f:
-        token = json.load(f)
-        dbx = dropbox.Dropbox(token["access_token"])
+    with open(config["dropbox"]["credentials_file"]) as f:
+        credentials = json.load(f)
+        app_key = credentials["app_key"]
+        app_secret = credentials["app_secret"]
+
+    dbx = None
+
+    if os.path.exists(config["dropbox"]["token_file"]):
+        with open(config["dropbox"]["token_file"], "r") as f:
+            token = json.load(f)
+            dbx = dropbox.Dropbox(
+                oauth2_access_token=token["access_token"],
+                oauth2_refresh_token=token["refresh_token"],
+                app_key=app_key,
+                app_secret=app_secret,
+            )
+
+            try:
+                dbx.check_and_refresh_access_token()
+            except Exception:
+                print("Dropbox token expired")
+                dbx = None
+
+    if not dbx:
+        flow = dropbox.DropboxOAuth2FlowNoRedirect(
+            consumer_key=app_key,
+            consumer_secret=app_secret,
+            locale="en_US",
+            token_access_type="offline",
+        )
+        url = flow.start()
+        print(f"Visit this URL and approve the app: {url}")
+        webbrowser.open(url)
+
+        result = flow.finish(input("Authorization code: "))
+
+        with open(config["dropbox"]["token_file"], "w") as f:
+            json.dump(
+                {
+                    "access_token": result.access_token,
+                    "refresh_token": result.refresh_token,
+                },
+                f,
+            )
+
+        dbx = dropbox.Dropbox(
+            oauth2_access_token=result.access_token,
+            oauth2_refresh_token=result.refresh_token,
+            app_key=app_key,
+            app_secret=app_secret,
+        )
 
     with open(config["ac_file"], "rb") as f:
         results = dbx.files_upload(
