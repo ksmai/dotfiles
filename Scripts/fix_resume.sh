2fc135f71e5f1b6ac71e42fe8497bfd63087c0ab
ksmai <24501900+ksmai@users.noreply.github.com>
2026-02-07 12:34:02 +0800

    no need pulseaudio hack after migrating to pipewire

--- Scripts/fix_resume.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-killall -q gammastep
-gammastep -P -O 3500 &
-
-if pacmd list-sinks | grep -q auto_null; then
-    pacmd set-card-profile alsa_card.pci-0000_00_1f.3 output:hdmi-stereo+input:analog-stereo
-    pacmd set-card-profile alsa_card.pci-0000_00_1f.3 output:hdmi-stereo
-fi
