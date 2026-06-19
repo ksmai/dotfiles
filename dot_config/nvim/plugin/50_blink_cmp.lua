2a34d46987f772f8ba9f799c33f85af2f057cb05
ksmai <24501900+ksmai@users.noreply.github.com>
2026-06-19 18:30:18 +0800

    simplify blink.cmp config

--- dot_config/nvim/plugin/50_blink_cmp.lua
+++ dot_config/nvim/plugin/50_blink_cmp.lua
@@ -1,7 +1,6 @@
 require("blink.cmp").setup({
 	keymap = {
 		preset = "super-tab",
-
 		["<Up>"] = {},
 		["<Down>"] = {},
 		["<C-space>"] = {},
@@ -9,55 +8,12 @@ require("blink.cmp").setup({
 		["<C-n>"] = { "show", "select_next", "fallback_to_mappings" },
 	},
 
-	appearance = {
-		-- Sets the fallback highlight groups to nvim-cmp's highlight groups
-		-- Useful for when your theme doesn't support blink.cmp
-		-- Will be removed in a future release
-		use_nvim_cmp_as_default = true,
-		-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-		-- Adjusts spacing to ensure icons are aligned
-		nerd_font_variant = "mono",
-	},
-
-	-- Default list of enabled providers defined so that you can extend it
-	-- elsewhere in your config, without redefining it, due to `opts_extend`
-	sources = {
-		default = { "lsp", "path", "snippets", "buffer" },
-
-		providers = {
-			lsp = {
-				score_offset = 3,
-			},
-
-			path = {
-				score_offset = 6,
-			},
-
-			snippets = {
-				score_offset = 0,
-			},
-
-			buffer = {
-				score_offset = 1,
-			},
-		},
-	},
-
-	-- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
-	-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-	-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-	--
-	-- See the fuzzy documentation for more information
-	fuzzy = { implementation = "prefer_rust_with_warning" },
-
 	signature = {
 		enabled = true,
 		window = { border = "single" },
 	},
 
 	completion = {
-		keyword = { range = "full" },
-
 		accept = {
 			auto_brackets = {
 				enabled = false,
@@ -152,18 +108,13 @@ require("blink.cmp").setup({
 			},
 		},
 
-		-- Show documentation when selecting a completion item
 		documentation = {
 			auto_show = true,
 			auto_show_delay_ms = 100,
 			window = { border = "single" },
 		},
 
-		-- Display a preview of the selected item on the current line
 		ghost_text = { enabled = true },
-
-		-- When false, will not show the completion window automatically when in a snippet
-		trigger = { show_in_snippet = false },
 	},
 
 	cmdline = {
