5695e5f5b75a9abce286768f4eb16d5a7c109f26
ksmai <24501900+ksmai@users.noreply.github.com>
2026-07-01 17:32:37 +0800

    fix mini ai cover_or_next issue

--- dot_config/nvim/plugin/60_mini.lua
+++ dot_config/nvim/plugin/60_mini.lua
@@ -6,8 +6,8 @@ local pairs = require("mini.pairs")
 
 mini_ai.setup({
 	n_lines = 9999,
-
 	search_method = "cover_or_next",
+	silent = true,
 
 	custom_textobjects = {
 		b = { { "%b()", "%b[]", "%b{}" }, "^.%s*().-()%s*.$" },
@@ -16,6 +16,20 @@ mini_ai.setup({
 		c = mini_ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),
 	},
 })
+
+-- The default cover_or_next might prefer a next match on the current line
+-- over a covering match that is not on the current line. We split the search
+-- into 2 find_textobject calls to avoid this issue. Note that silent has to
+-- be true in order to avoid some inaccurate error messages.
+local find_textobject = mini_ai.find_textobject
+mini_ai.find_textobject = function(ai_type, id, opts)
+	if opts.n_times ~= 1 or opts.search_method ~= "cover_or_next" then
+		return find_textobject(ai_type, id, opts)
+	end
+
+	return find_textobject(ai_type, id, vim.tbl_deep_extend("force", opts, { search_method = "cover" }))
+		or find_textobject(ai_type, id, vim.tbl_deep_extend("force", opts, { search_method = "next" }))
+end
 
 splitjoin.setup({
 	mappings = {
